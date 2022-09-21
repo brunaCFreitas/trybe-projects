4618a91e1a9dc992c635d06bb17039c1d1fb49b7
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-09-21 19:19:51 -0300

    Adiciona collor-guess

---
+++ fundamentos/color-guess/README.md
@@ -0,0 +1,10 @@
+# Projeto Adivinhe a Cor
+
+## Descrição do Projeto
+
+Neste projeto `bônus` do módulo de **fundamentos** nós tivemos a missão de criar um jogo, onde eram sorteadas cores aleatórias e o usuário deveria escolher uma cor, na tentativa de ser a cor também escolhida pelo <del>computador</del>.
+
+
+### Veja abaixo o funcionamento do meu projeto:
+
+[collorguess.webm](https://user-images.githubusercontent.com/80068419/191620267-3a5f13d1-983c-48e9-a15e-b4e9150d2507.webm)
---
+++ fundamentos/color-guess/script.js
@@ -0,0 +1,84 @@
+const paragraph = document.getElementById('rgb-color');
+const paletteColors = document.getElementById('palette-colors');
+const answer = document.getElementById('answer');
+const buttonRestart = document.getElementById('reset-game');
+const scoreElement = document.getElementById('score');
+
+let clickCounter = 0;
+
+function addPoint() {
+  const points = localStorage.getItem('points') || 0;
+  localStorage.setItem('points', +points + 3);
+}
+
+function updatePoints() {
+  const points = localStorage.getItem('points') || 0;
+  scoreElement.innerText = points;
+}
+
+function generateRandomColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  const color = `rgb(${red}, ${green}, ${blue})`;
+
+  return color;
+}
+
+function checkColors(event) {
+  clickCounter += 1;
+
+  if (clickCounter > 1) {
+    return;
+  }
+
+  const targetColor = event.target.style.backgroundColor;
+  const isCorrect = targetColor === paragraph.innerText;
+
+  if (isCorrect) {
+    answer.innerText = 'Acertou!';
+    addPoint();
+    updatePoints();
+    return;
+  }
+
+  answer.innerText = 'Errou! Tente novamente!';
+}
+
+function addRandomColor(colors) {
+  const colorPalette = document.createElement('div');
+  colorPalette.className = 'ball';
+  const color = generateRandomColor();
+  colorPalette.style.backgroundColor = color;
+  paletteColors.addEventListener('click', checkColors);
+  colors.push(color);
+  paletteColors.appendChild(colorPalette);
+  paragraph.innerText = color;
+}
+
+function createPaletteColors() {
+  const colors = [];
+
+  for (let index = 0; index < 6; index += 1) {
+    addRandomColor(colors);
+  }
+
+  colors.sort(() => 0.5 - Math.random());
+  paragraph.innerText = colors.shift();
+}
+
+function restore() {
+  answer.innerText = 'Escolha uma cor';
+  paletteColors.innerHTML = '';
+  clickCounter = 0;
+  createPaletteColors();
+  updatePoints();
+}
+
+createPaletteColors();
+updatePoints();
+
+buttonRestart.addEventListener('click', () => {
+  restore();
+});
